2cf5a1aea3dc64f672def52ac1eca201b6b48f29
Bernard jeff <bernardjeff2525@gmail.com>
2023-09-28 11:59:23 +0800

    add: new and create method for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
   def index
+    # start_date = '2023-09-28'
+    # end_date = '2023-09-30'
     @posts = Post.all
+    # @posts = @posts.where(title: 'My sidfdfxth post')
+    # @posts = @posts.where(created_at: start_date..end_date)
+    # @posts = @posts.order(created_at: :desc) # desc / asc
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :content))
+    if @post.save
+      flash[:notice] = 'Post created successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post create failed'
+      render :new, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index', to: 'posts#index'
+  # get 'posts/index', to: 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'welcome#index'
   get :about, to: 'welcome#about'
+
+  resources :posts
 end
