5f4a6828fe23cbbaf8d06961e5e39687946d026e
Bernard jeff <bernardjeff2525@gmail.com>
2023-10-03 11:39:15 +0800

    add: Category Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   validates :title, :content, presence: true
 
   has_many :comments
+
+  has_many :post_category_ships
+  has_many :categories, through: :post_category_ships
 end
