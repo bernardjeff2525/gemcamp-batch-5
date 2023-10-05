983fc650dabf1c7ea65fc5f057052840b5e83c83
Bernard jeff <bernardjeff2525@gmail.com>
2023-10-05 11:04:51 +0800

    modify: seed to use faker for post and category model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
-100.times do |i|
-  Post.create(title: "numbered post #{i}", content: "numbered post content #{i}")
+5.times do
+  Category.create(name: Faker::ProgrammingLanguage.name)
+end
+
+50.times do |i|
+  post = Post.new
+  post.title = Faker::Lorem.sentence
+  post.content = Faker::Lorem.paragraph(sentence_count: 4)
+  post.categories << Category.all.sample
+  post.save
+
+  10.times do
+    post.comments.create(content: Faker::Lorem.paragraph(sentence_count: 4))
+  end
 end
