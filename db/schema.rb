644fd5f5061f4bb426ba8078a6268e95a6f17d81
Bernard jeff <bernardjeff2525@gmail.com>
2023-10-05 11:48:47 +0800

    add: reference user to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_031012) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_033432) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_031012) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
