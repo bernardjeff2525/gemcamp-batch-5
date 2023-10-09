6a720323725368bf97d02cff3961af89a523f61c
Bernard jeff <bernardjeff2525@gmail.com>
2023-10-09 10:34:58 +0800

    bug: add user reference in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,20 @@
   Category.create(name: Faker::ProgrammingLanguage.name)
 end
 
+10.times do
+  User.create(email: Faker::Internet.email, password: Faker::Alphanumeric.alpha(number: 10))
+end
+
 50.times do |i|
   post = Post.new
   post.title = Faker::Lorem.sentence
   post.content = Faker::Lorem.paragraph(sentence_count: 4)
   post.categories << Category.all.sample
+  post.user = User.all.sample
   post.save
 
   10.times do
-    post.comments.create(content: Faker::Lorem.paragraph(sentence_count: 4))
+    post.comments.create(content: Faker::Lorem.paragraph(sentence_count: 4),
+                         user: User.all.sample)
   end
 end
