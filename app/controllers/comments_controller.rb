92b25f3ece338ac4797a1a31407f0b512e1dfbee
Bernard jeff <bernardjeff2525@gmail.com>
2023-10-09 10:02:01 +0800

    modify: authenticate user for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: :index
   before_action :set_post
-  before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :set_comment, :validate_comment_owner, only: [:edit, :update, :destroy]
+  # before_action :validate_comment_owner, only: [:edit, :update, :destroy]
 
   def index
-    @comments = @post.comments
+    @comments = @post.comments.includes(:user)
+                     .order(created_at: :desc)
   end
 
   def new
@@ -12,6 +15,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
     if @comment.save
       flash[:notice] = 'Comment created successfully'
       redirect_to post_comments_path(@post)
@@ -49,5 +53,12 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def validate_comment_owner
+    return if @comment.user == current_user
+
+    flash[:alert] = 'This comment does not belongs to you'
+    redirect_to post_comments_path(@post)
   end
 end
