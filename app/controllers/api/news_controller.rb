960285269f186af845b5e9176fefb20fb1b84756
Bernard jeff <bernardjeff2525@gmail.com>
2023-10-16 10:33:38 +0800

    add: category filter to new api page

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -1,9 +1,13 @@
 class Api::NewsController < ApplicationController
   def index
-    params = { 'apiKey': '42a6e108bb4548cc8d3179d86d554a49', country: 'ph' }
-    url = 'https://newsapi.org/v2/top-headlines'
+    @categories = %w[business entertainment general health science sports technology]
+    api_params = {
+      'apiKey': '42a6e108bb4548cc8d3179d86d554a49',
+      country: 'ph'
+    }
 
-    @response = RestClient.get url, params: params
+    url = 'https://newsapi.org/v2/top-headlines'
+    @response = RestClient.get url, params: api_params
     @articles = JSON.parse(@response)['articles']
   end
 end
