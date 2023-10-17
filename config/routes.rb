59489d0aa6e8a6858f871c2421b2cf9dc0f89df1
Bernard jeff <bernardjeff2525@gmail.com>
2023-10-17 11:35:33 +0800

    add: routes for api ph location

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,20 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :news, only: :index
+    namespace :v1 do
+      resources :regions, only: [:index, :show] do
+        resources :provinces, only: :index
+      end
+
+      resources :provinces, only: [:index, :show] do
+        resources :cities, only: :index
+      end
+
+      resources :cities, only: [:index, :show] do
+        resources :barangays, only: :index
+      end
+
+      resources :barangays, only: [:index, :show]
+    end
   end
 end
