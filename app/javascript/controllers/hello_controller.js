b8d4792448056be284403000a835621f7ca26f06
Bernard jeff <bernardjeff2525@gmail.com>
2023-10-20 10:58:57 +0800

    add: create sample for hello controller js

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["name"]
+
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "Hello World!"
+    // console.log(`Hello, Stimulus! ${this.element}`)
+    // console.log("Hello, Stimulus", this.element)
+  }
+
+  greet() {
+    console.log(this.nameTarget)
+    console.log("Hello, Stimulus", this.nameTarget.value)
+    alert(`Hello, Stimulus ${this.nameTarget.value}`)
+    this.nameTarget.value = ""
   }
 }
