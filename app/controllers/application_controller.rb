6bcca48f7cfaa3773b453d4cbf53eee2cdd24f55
Bernard jeff <bernardjeff2525@gmail.com>
2023-10-26 10:25:02 +0800

    add: i18n for post model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
   def after_sign_up_path_for(resource)
     after_sign_in_path_for(resource) if is_navigational_format?
   end
